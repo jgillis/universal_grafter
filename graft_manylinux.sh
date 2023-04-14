04e9df8079124ffe943de700bc1b6c49047fd989
jgillis <joris.gillis42@gmail.com>
2023-04-14 07:55:05 +0200

    Update graft_manylinux.sh

--- graft_manylinux.sh
+++ graft_manylinux.sh
@@ -10,5 +10,5 @@ pushd /tmp/auditwheel && $PYTHONDIR/python setup.py install && popd
 
 echo "1: $1"
 $PYTHONDIR/auditwheel show $1
-$PYTHONDIR/auditwheel repair -L . --exclude libknitro.so --exclude libworhp.so --no-update-tags -w . $1
+$PYTHONDIR/auditwheel repair -L . --exclude libknitro.so --exclude libworhp.so --exclude libhsl.so --exclude libsnopt7.so --no-update-tags -w . $1
 
