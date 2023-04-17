33bafdbef6381b6a761ed6a7e217773157a4dd88
Joris Gillis <joris.gillis42@gmail.com>
2023-04-17 22:27:56 +0200

    updates

--- graft_linux.sh
+++ graft_linux.sh
@@ -6,5 +6,5 @@ docker run --rm ghcr.io/jgillis/$1:production > /tmp/dockcross
 chmod +x /tmp/dockcross
 cp $SCRIPT_DIR/graft_manylinux.sh .
 echo "2: $2"
-/tmp/dockcross ./graft_manylinux.sh $2
+/tmp/dockcross ./graft_manylinux.sh $2 $3
 rm graft_manylinux.sh
--- graft_manylinux.sh
+++ graft_manylinux.sh
@@ -9,6 +9,13 @@ git -C /tmp clone -b main https://github.com/jgillis/auditwheel.git
 pushd /tmp/auditwheel && $PYTHONDIR/python setup.py install && popd
 
 echo "1: $1"
-$PYTHONDIR/auditwheel show $1 || echo fail
-$PYTHONDIR/auditwheel repair -L . --exclude libknitro.so --exclude libworhp.so --exclude libhsl.so --exclude libsnopt7.so --exclude libcasadi-tp-openblas.so.0 --no-update-tags -w . $1
+$PYTHONDIR/auditwheel show $1
+export LD_LIBRARY_PATH=`pwd`/casadi:`pwd`/dummy/casadi
+excludes=""
+for lib in $(echo $2 | tr ":" "\n")
+do
+    excludes="$excludes --exclude ${lib}.so"
+done
+
+$PYTHONDIR/auditwheel -vv repair -L . $excludes --no-update-tags -w . $1
 
