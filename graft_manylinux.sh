aa48158d7ef017c6e84ce6554228795b6fcad2a2
Joris Gillis <joris.gillis42@gmail.com>
2023-04-18 00:07:47 +0200

    test

--- graft_manylinux.sh
+++ graft_manylinux.sh
@@ -5,7 +5,7 @@ PYTHONDIR=/opt/python/cp39-cp39/bin
 
 $PYTHONDIR/pip install pyelftools wheel==0.31.1
 #git -C /tmp clone -b fix https://github.com/jgillis/auditwheel/
-git -C /tmp clone -b main https://github.com/jgillis/auditwheel.git
+git -C /tmp clone -b test https://github.com/jgillis/auditwheel.git
 pushd /tmp/auditwheel && $PYTHONDIR/python setup.py install && popd
 
 echo "1: $1"
